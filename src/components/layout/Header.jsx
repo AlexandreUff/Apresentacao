a51b5af80b638f679e41c905b850f96148c776fe
AlexandreUff <alexandre_magno@id.uff.br>
2022-05-01 22:32:54 -0300

    Primeiro Commit

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 export default function Header(){
+
+    function effectBg(){
+        const Header = document.querySelector('header');
+        console.log(Header);
+        Header.classList.add("bg-image-effect");
+    }
+
+    
+
+
     return (
-        <header >
+        <header>
             <div className="ground">
                 <img src="/images/img-main.png" alt="Foto de Alexandre Magno" title="Alexandre Magno" />
                 <h1>ALEXANDRE MAGNO DIAS</h1>
             </div>
-        </header>)
+            {
+                setTimeout(()=>{
+                    effectBg();
+                },100)
+            }
+        </header>
+        )
 }
