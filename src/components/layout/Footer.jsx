3ddbdc6e09ecf1acdc8a703a7021ebb7c0b40ee5
AlexandreUff <alexandre_magno@id.uff.br>
2023-05-30 16:35:49 -0300

    set new features

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { IcoGHBs } from "../items/Icones";
+import { IcoGHBs, IcoLIn, IcoPDF } from "../items/Icones";
 
 export default function Footer(){
     return (
         <footer>
-            <div className="media">
-                <a href="https://github.com/AlexandreUff" title="Acesso ao GitHub de Alexandre Magno" target="blank" >
-                    <IcoGHBs/>
-                </a>
-            </div>
+            <nav className="media">
+                <ul>
+                    <li>
+                        <a href="https://github.com/AlexandreUff" title="Acesso ao GitHub de Alexandre Magno" target="blank" >
+                            <IcoGHBs/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/alexandre-magno-dias-pinto-3659b5225/" title="Acesso ao LinkedIn de Alexandre Magno" target="blank" >
+                            <IcoLIn/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://drive.google.com/file/d/1VY_KJL4z3YhXYYhJx04lPM3FpGPX3Uot/view?usp=share_link" title="Acesso ao currículo de Alexandre Magno" target="blank" >
+                            <IcoPDF/>
+                        </a>
+                    </li>
+                </ul>
+                
+                
+            </nav>
             <div>
                 <small>Imagens: Samuel Walker; Freepik; dDara.</small>
                 <br />
