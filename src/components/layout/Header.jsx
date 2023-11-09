9958ffe43373e2eafe3b8ca7f050385f5215232c
AlexandreUff <alexandre_magno@id.uff.br>
2023-11-08 22:11:45 -0300

    feat: add new main img

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -14,7 +14,7 @@ export default function Header(){
 
     return (
         <header>
-            <img src="/images/img-main.png" alt="Foto de Alexandre Magno" title="Alexandre Magno" />
+            <img src="/images/img-main-pr.png" alt="Foto de Alexandre Magno" title="Alexandre Magno" />
             <div className="video-content">
                 <video autoPlay muted loop id="video-background">
                     <source src="/video/ground_video.mp4" type="video/mp4" />
