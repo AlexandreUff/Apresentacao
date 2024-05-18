e2288f6a3247caab7091b1edf73514c5fb7d988b
AlexandreUff <alexandre.magno.wrk@gmail.com>
2024-05-18 11:27:54 -0300

    feat: add new sources to modal

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -10,8 +10,73 @@ export default function Footer(){
     return (
         <footer>
             <Modal isOpen={openModal} isClose={setOpenModal}>
-                <div style={{backgroundColor: 'red', width: '50px', height: '50px'}}>
-                    Teste
+                <div
+                    style={
+                        {
+                            backgroundColor: '#6EBF8B',
+                            border: '4px solid #151D3B',
+                            borderRadius: '10px',
+                            width: '412px',
+                        }
+                    }
+                >
+                    <div
+                        style={
+                            {
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                backgroundColor: '#151D3B',
+                                width: '100%',
+                                height: '40px',
+                                margin: '0px',
+                                fontSize: '1.5rem',
+                                fontWeight: 'bold'
+                            }
+                        }
+                    >
+                        Escolha o idioma do currículo
+                    </div>
+                    <div
+                        style={
+                            {
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                color: '#151D3B',
+                                fontWeight: 'bold',
+                                fontSize: '1.5rem',
+                                padding: '20px'
+                                }
+                            }
+                    >
+                        <ul>
+                            <li style={{marginBottom:'10px'}}>
+                                <a
+                                    href="https://drive.google.com/file/d/1mjRuG7yA4TX5dmp-JTLBlfp3jKBP9HlL/view?usp=drive_link"
+                                    title="Currículo de Alexandre magno em português"
+                                    target="blank"
+                                    style={{
+                                        color: '#151D3B'
+                                    }}
+                                >
+                                    🇧🇷 Português Brasil (PDF)
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://drive.google.com/file/d/1qN8R-2V3dzccoUvGkv_TXMq7VZl5DWkD/view?usp=drive_link"
+                                    title="Alexandre Magno resume in english"
+                                    target="blank"
+                                    style={{
+                                        color: '#151D3B'
+                                    }}
+                                >
+                                    🇺🇸 Inglês (PDF)
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </Modal>
             <nav className="media">
@@ -27,11 +92,7 @@ export default function Footer(){
                         </a>
                     </li>
                     <li>
-                        <a
-                            /* href="https://drive.google.com/file/d/1mjRuG7yA4TX5dmp-JTLBlfp3jKBP9HlL/view?usp=drive_link" */
-                            title="Acesso ao currículo de Alexandre Magno"
-                            target="blank"
-                            onClick={() => setOpenModal(true)}
+                        <a title="Acesso ao currículo de Alexandre Magno" onClick={() => setOpenModal(true)}
                         >
                             <IcoPDF/>
                         </a>
